15ac0ec3e2597f3f89de1685848d0d470ca37ded
Akshay Sanjeev <asanjeevkeloth@gmail.com>
2025-04-02 00:22:35 +0530

    Refactor HMM generation function and add Jupyter notebook for assignment 2

--- HW-2/hw-2-fns.py
+++ HW-2/hw-2-fns.py
@@ -1,33 +1,23 @@
 import numpy as np
 
-def hmm_gen(N, S, T, a, e):
+def hmm_gen(S, T, a, e):
     '''
-    N= length of hidden markov sequence desired to be generated,
     S = observable states. e.g, {Head, Tail}
-    T = hidden states. e.g, {Fair, Biased}
+    T = hidden states. e.g, {0(init/term), 1(Fair), 2(Biased)}
     a = transition probabilites for hidden states
     e = emission probabilities of hidden states
+    OUTPUT:
+    pi = hidden state chain
+    x = observable state chain
     '''
-    
-    hdn_seq = np.zeros(N, dtype=int)
-    obs_seq = np.zeros(N, dtype=str)
-    hdn_seq[0] = np.random.choice(T[1:], p=a[0, 1: len(T)])
-    obs_seq[0] = np.random.choice(S, p=e[hdn_seq[0]-1, :])
+    T =  np.arange(T)
+    pi = []
+    x = []
+    pi.append(np.random.choice(T[1:], p=a[0, 1: len(T)]))
+    x.append(np.random.choice(S, p=e[pi[0]-1, :]))
+    while pi[-1] != 0:
+        pi.append(np.random.choice(T, p=a[pi[-1], :]))
+        x.append(np.random.choice(S, p=e[pi[-1]-1, :]))
+        
+    return pi, x
 
-    for i in range(1, N):
-        hdn_seq[i] = np.random.choice(T[1:], p=a[])
-
-
-    
-
-
-S = ['H', 'T']
-T = np.arange(3)
-
-a = np.array([[  0,  .5,  .5], #tranisiton proba
-              [.01, .94, .05],
-              [.01, .05, .94]])
-e = np.array([[.5, .5],
-              [.8, .2]])
-
-hmm_gen(2, S, T, a, e)
