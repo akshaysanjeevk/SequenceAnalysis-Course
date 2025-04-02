a59ac32eb49d69b9db1bf6864e3666d152d805e9
Akshay Sanjeev <asanjeevkeloth@gmail.com>
2025-04-03 00:26:35 +0530

    Add Viterbi function; update notebook for clarity

--- HW-2/hw-2-fns.py
+++ HW-2/hw-2-fns.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.random import choice as choose
+
 
 def hmm_gen(S, T, a, e):
     '''
@@ -13,11 +15,31 @@ def hmm_gen(S, T, a, e):
     T =  np.arange(T)
     pi = []
     x = []
-    pi.append(np.random.choice(T[1:], p=a[0, 1: len(T)]))
-    x.append(np.random.choice(S, p=e[pi[0]-1, :]))
+    pi.append(choose(T[1:], p=a[0, 1: len(T)]))
+    x.append(choose(S, p=e[pi[0]-1, :]))
     while pi[-1] != 0:
-        pi.append(np.random.choice(T, p=a[pi[-1], :]))
-        x.append(np.random.choice(S, p=e[pi[-1]-1, :]))
-        
+        pi.append(choose(T, p=a[pi[-1], :]))
+        x.append(choose(S, p=e[pi[-1]-1, :]))
+    pi = [int(p) for p in pi]
+    x = [str(q) for q in x]
     return pi, x
+
+    
+def Viterbi(x, S, T, a, e):#x is obs, h is hid
+    T = np.arange(T)
+    obstates = {'H' : 0, 'T' : 1}
+    v = np.zeros((len(x), len(T)))
+    v[:, 0] = 0
+    # v[0, :] = a[0, :]
+    path = []
+    for xi in range(len(x)):
+        for hi in range(1, len(T)-1):
+            p_trans = a[hi-1, hi]
+            p_emssn = e[hi, obstates[x[xi]]]
+            v[xi, hi] = v[xi-1, hi-1]*p_trans*p_emssn
+        path.append((np.max(v[xi, :]), np.argmax(v[xi, :])))
+    print(v)   
+    path = [(float(a), int(b)) for a, b in path]
+    return path
+
 
