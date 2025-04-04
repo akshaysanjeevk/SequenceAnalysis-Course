384390524eadc34f434841fa4509d3310f925274
askay-sanjeev <asanjeevkeloth@gmail.com>
2025-04-04 19:51:53 +0530

    Add Backward function and update notebook with Forward-Backward algorithm

--- HW-2/hw2_fns.py
+++ HW-2/hw2_fns.py
@@ -25,20 +25,18 @@ def hmm_gen(S, T, a, e):
     return pi, x
 
     
+    
 def Viterbi(x, S, T, a, e):#x is obs, h is hid
     obsstates = {S[0]: 0, S[1] : 1}
     v = np.zeros((len(x), T-1))
-    v[:, 0] = 0
     v[0, :] = a[0, 1:]
     path = []
     for xi in range(1,len(x)):
         for hi in range(T-1):
-            k = []
-            for hi2 in range(T-1):
-                p_trans = a[hi, hi2]
-                p_emssn = e[hi-1, obsstates[x[xi]]]
-                k.append(v[xi-1, hi2]*p_trans*p_emssn)
-            v[xi, hi] = np.max(k)
+            v[xi, hi] = np.max([
+                v[xi-1, i] * a[i, hi] * e[hi, obsstates[x[xi]]]
+                for i in range(T-1)
+            ])
         path.append(np.argmax(v[xi, :]))
     return [int(i) for i in path]
 
@@ -49,40 +47,27 @@ def match(pi1, pi2):
             score+=1
     return score/len(pi1)
 
-
 def Forward(x, S, T, a, e):
-    T = np.arange(T)
-    obsstates = {S[0]: 0, S[1] : 1}
-    alpha = np.zeros((len(x), len(T)-1))
-    alpha[:, 0] = 0
-    alpha[0, :] = a[0, 1:]
-    path = []
-    for xi in range(1,len(x)):
-        for hi in range(len(T)-1):
-            k = []
-            for hi2 in range(len(T)-1):
-                p_trans = a[hi, hi2]
-                p_emssn = e[hi-1, obsstates[x[xi]]]
-                k.append(alpha[xi-1, hi2]*p_trans*p_emssn)
-            alpha[xi, hi] = np.sum(k)
-    likelihood =  np.sum(alpha[-1, :])
-    return likelihood
+    obs_index = {obs: i for i, obs in enumerate(S)}
+    alpha = np.zeros((len(x), T-1))
+    for i in range(T-1):
+        alpha[0, i] = a[0, i+1] * e[i, obs_index[x[0]]]
+    for xi in range(1, len(x)):
+        obs_idx = obs_index[x[xi]]
+        for hi in range(T-1):
+            alpha[xi, hi] = np.sum(
+                alpha[xi-1, :] * a[1:, hi+1] * e[hi, obs_idx])
+    return np.sum(alpha[-1, :])
 
-# def Backward(x, S, T, a, e):
-#     T = np.arange(T)
-#     obsstates = {S[0]: 0, S[1] : 1}
-#     alpha = np.zeros((len(x), len(T)-1))
-#     alpha[:, 0] = 0
-#     alpha[0, :] = a[0, 1:]
-#     path = []
-#     for xi in range(1,len(x)):
-#         for hi in range(len(T)-1):
-#             k = []
-#             for hi2 in range(len(T)-1):
-#                 p_trans = a[hi, hi2]
-#                 p_emssn = e[hi-1, obsstates[x[xi]]]
-#                 k.append(alpha[xi-1, hi2]*p_trans*p_emssn)
-#             alpha[xi, hi] = np.sum(k)
-#     likelihood =  np.sum(alpha[-1, :])
-#     return likelihood
+def Backward(x, S, T, a, e):
+    obs_index = {s: i for i, s in enumerate(S)}
+    N = T - 1
+    beta = np.zeros((len(x), T))
+    beta[-1, 1:] = 1  
+    for xi in reversed(range(len(x)-1)):
+        for hi in range(1, T):
+            beta[xi, hi] = np.sum(
+                beta[xi+1, 1:] * a[hi, 1:] * e[:, obs_index[x[xi+1]]])
+    likelihood = np.sum(a[0, 1:] * e[:, obs_index[x[0]]] * beta[0, 1:])
+    return likelihood
 
